4870bac1a3d65d7e9e490ba0b4a7d1e4c9c9dcdd
lamvanhieu152-cmyk <lamvanhieu152@gmail.com>
2026-09-10 02:04:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vstep-vocab-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE='vstep-vocab-v3';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
